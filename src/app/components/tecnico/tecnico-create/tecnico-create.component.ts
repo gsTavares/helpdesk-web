fb738e3988358cccfb276c5fde8ec9a18f35a4d6
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-26 15:11:55 -0300

    cadastro de tecnicos

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/Tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,18 +12,48 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico = {} as Tecnico;
+
   nome: FormControl = new FormControl(null, Validators.minLength(3))
   cpf: FormControl = new FormControl(null, Validators.required)
   email: FormControl = new FormControl(null, Validators.email)
   senha: FormControl = new FormControl(null, Validators.minLength(3))
 
-  constructor() { }
+  constructor(
+    private tecnicoService: TecnicoService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.tecnico.perfis = []
+  }
+
+  create(): void {
+    this.tecnicoService.create(this.tecnico).subscribe({
+      next: () => {
+        this.toastr.success('Técnico cadastrado com sucesso!', 'Cadastro');
+        this.router.navigate(['tecnicos']);
+      },
+      error: (ex) => {
+        if(ex.error.errors) {
+          ex.error.errors.forEach((element: { message: string | undefined; }) => {
+              this.toastr.error(element.message);
+          });
+        } else {
+          this.toastr.error(ex.error.message);
+        }
+      }
+    });
+  }
+
+  addPerfil(perfil: any): void {
+    this.tecnico.perfis.includes(perfil) ? this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1)
+      : this.tecnico.perfis.push(perfil);
   }
 
   validaCampos(): boolean {
-    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    return (this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid)
   }
 
 }
