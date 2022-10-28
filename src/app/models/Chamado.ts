86d9c686f087d5128854f1672e721f63a0445243
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-28 08:29:08 -0300

    interface Chamado criada

---
+++ src/app/models/Chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+    id?: number,
+    dataAbertura?: string,
+    dataFechamento: string,
+    prioridade: string,
+    status: string,
+    titulo: string,
+    descricao: string,
+    tecnico: any,
+    cliente: any,
+    nomeCliente: string,
+    nomeTecnico: string
+}
