a85349ce60fb43c590bd819da21eb1483a4e0857
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-28 09:30:38 -0300

    service e filtro para component chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from 'src/app/models/Chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-list',
@@ -10,35 +11,60 @@ import { Chamado } from 'src/app/models/Chamado';
 })
 export class ChamadoListComponent implements OnInit {
 
-  ELEMENT_DATA: Chamado[] = [
-    {
-      id: 1,
-      dataAbertura: '21/06/2021',
-      dataFechamento: '21/06/2021',
-      prioridade: 'ALTA',
-      status: 'ANDAMENTO',
-      titulo: 'Chamado 1',
-      descricao: 'Teste chamado 1',
-      tecnico: 1,
-      cliente: 1,
-      nomeCliente: 'Teste',
-      nomeTecnico: 'Teste 2'
+  ELEMENT_DATA: Chamado[] = [];
+  FILTERED_DATA: Chamado[] = [];
 
-    }
-  ];
   displayedColumns = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'actions'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  constructor(
+    private chamadoService: ChamadoService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.chamadoService.findAll().subscribe({
+      next: (response) => {
+        this.ELEMENT_DATA = response;
+        this.dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+        this.dataSource.paginator = this.paginator;
+      }
+    })
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    switch (status) {
+      case 0: return 'ABERTO'
+      case 1: return 'EM ANDAMENTO'
+      case 2: return 'ENCERRADO'
+      default: return '-'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    switch (prioridade) {
+      case 0: return 'BAIXA'
+      case 1: return 'MÉDIA'
+      case 2: return 'ALTA'
+      default: return '-'
+    }
+  }
+
+  filterByStatus(status: any) {
+    const listChamado = [...this.ELEMENT_DATA];
+    this.FILTERED_DATA = listChamado.filter(element => element.status == status);
+    this.dataSource = new MatTableDataSource<Chamado>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
   }
 
 }
